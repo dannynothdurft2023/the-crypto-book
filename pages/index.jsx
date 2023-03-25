15f5fae039344d8ecde85eed64e655d486051f2e
Danny Nothdurft <Danny.Nothdurft@icloud.com>
2023-03-25 17:37:20 +0100

    Test

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,7 +37,7 @@ export default function Home() {
               mich darauf, Sie als Leserinnen und Leser begrüßen zu dürfen.
             </p>
 
-            <button>Zu Amazon Shop</button>
+            <button>Zu Amazon</button>
           </div>
         </div>
       </main>
